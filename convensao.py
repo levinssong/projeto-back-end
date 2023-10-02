e224faf7512f7d3faa01f3789ac5544ee7a7a9eb
levinssong <130846884+levinssong@users.noreply.github.com>
2023-10-02 20:43:48 -0300

    Add files via upload

---
+++ convensao.py
@@ -0,0 +1,9 @@
+binario = input("Digite um número binário: ")
+decimal = int(binario, 2)
+
+print(f"Esse número em decimal é {decimal}")
+
+decimal = int(input("Digite um número decimal: "))
+binario = bin(decimal)
+
+print(f"Esse número em binario é {binario}")
