128792a8025b28955a60b98ce6057820908b62bf
levinssong <130846884+levinssong@users.noreply.github.com>
2023-10-04 18:08:35 -0300

    Delete convensao.py

--- convensao.py
+++
@@ -1,9 +0,0 @@
-binario = input("Digite um número binário: ")
-decimal = int(binario, 2)
-
-print(f"Esse número em decimal é {decimal}")
-
-decimal = int(input("Digite um número decimal: "))
-binario = bin(decimal)
-
-print(f"Esse número em binario é {binario}")
